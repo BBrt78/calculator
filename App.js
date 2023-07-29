e357f5c5a559624c4d3cd7b803ac48ea40a938b5
BBrt78 <103853975+BBrt78@users.noreply.github.com>
2023-07-29 17:28:51 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,65 +4,43 @@ import {Input} from "./input.js"
 import {Output} from "./output.js"
 
 function App() {
-
-    const [output, setOutput] = useState([]);
-    const [input, setInput] = useState([]);
     
-    // useEffect(() => {
-    //     setOutput([...output, input]);
-    //   }, [input]);
-
-    const addInput = (e) => {
-        if (input.includes(".") && e.target.value === ".") {
-        } else if (e.target.value === "+") {
-            setInput([...input, e.target.value]);
-            setOutput([...output, input])
-            // setInput([])
-        } else {
-            setInput([...input, e.target.value]);
-            
-        } console.log(input)
-    }
-
-    const clearAll = () => {
-        setOutput([]);
-        setInput([])
-    }
-
-    const clear = () => {
-    }
+    const addSign = () => {
+        
+    };
+    const [output, setOutput] = useState("output");
+    const [input, setInput] = useState("input");
    
     return (
         <div className="container">
             <div className="container1">
-                <Output value={output}/>
-                <Input value={input}/>
+                <Output value={output} />
+                <Input value={input} />
             </div>
 
             <div className="container2">
-                <button className="ca button-a" value="CA" onClick={clearAll}>CA</button>
-                <button className="c button-a" value="C" onClick={clear}>C</button>
-                <button className="button-a" value="%" onClick={addInput}>%</button>
-                <button className="button-a" value="+" onClick={addInput}>+</button>
+                <button className="ca button-a" value="CA">CA</button>
+                <button className="c button-a" value="C">C</button>
+                <button className="button-a" value="%">%</button>
+                <button className="button-a" value="+">+</button>
 
-                <button className="button-b" value="7" onClick={addInput}>7</button>
-                <button className="button-b" value="8" onClick={addInput}>8</button>
-                <button className="button-b" value="9" onClick={addInput}>9</button>
-                <button className="button-b" value="-" onClick={addInput}>-</button>
+                <button className="button-b" value="7" onClick={addSign}>7</button>
+                <button className="button-b" value="8" onClick={addSign}>8</button>
+                <button className="button-b" value="9" onClick={addSign}>9</button>
+                <button className="button-b" value="-" onClick={addSign}>-</button>
 
-                <button className="button-c" value="4" onClick={addInput}>4</button>
-                <button className="button-c" value="5" onClick={addInput}>5</button>
-                <button className="button-c" value="6" onClick={addInput}>6</button>
-                <button className="button-c" value="*" onClick={addInput}>*</button>
+                <button className="button-c" value="4" onClick={addSign}>4</button>
+                <button className="button-c" value="5" onClick={addSign}>5</button>
+                <button className="button-c" value="6" onClick={addSign}>6</button>
+                <button className="button-c" value="*" onClick={addSign}>*</button>
 
-                <button className="button-d" value="1" onClick={addInput}>1</button>
-                <button className="button-d" value="2" onClick={addInput}>2</button>
-                <button className="button-d" value="3" onClick={addInput}>3</button>
-                <button className="button-d" value="/" onClick={addInput}>/</button>	
+                <button className="button-d" value="1" onClick={addSign}>1</button>
+                <button className="button-d" value="2" onClick={addSign}>2</button>
+                <button className="button-d" value="3" onClick={addSign}>3</button>
+                <button className="button-d" value="/" onClick={addSign}>/</button>	
 		
-                <button className="button-e" value="0" onClick={addInput}>0</button>
-                <button className="button-e" value="." onClick={addInput}>.</button>
-                <button className="button-e" value="+/-" onClick={addInput}>+/-</button>
+                <button className="button-e" value="0" onClick={addSign}>0</button>
+                <button className="button-e" value="+/-" onClick={addSign}>+/-</button>
                 <button className="= button-e" value="=">=</button>
             </div>	
 		</div>
